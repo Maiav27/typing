6a5af2f066a63a15dfec35d94dd016e1d6b88967
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-09-15 11:46:34 -0300

    add prject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <div className='valid-keys'>
+          <span className="matched">emer</span>
+          <span className="remainder">son</span>
+       </div>
+       <div className="typed-keys">dfsfsds</div>
+       <div className="completed-words">
+          <ol>
+            <li>
+            cidade
+            </li>
+            <li>rua</li>
+          </ol>
+       </div>
     </div>
   );
 }
