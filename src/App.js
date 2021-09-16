259edb04894493752e20f550af1d2fbacc6cf09e
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-09-15 21:22:25 -0300

    add UseEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import wordList from './resource/words.json'
+import { getWord } from './resource/func'
+import Word from './Word'
+
+const MAX_TYPED_KEYS = 30
 function App() {
+
   const [typedKeys, setTypedKeys] = useState([])
+  const [word, setWord] = useState('')
+  const [validKeys, setValidKeys] = useState([])
+    useEffect(() =>{
+       setWord(getWord(wordList))
+    }, [])
   const handleKeyDown = (event) =>{
     event.preventDefault()
     const {key} = event
-    setTypedKeys((preventTypedKeys) => [...preventTypedKeys, key]) //vou deixar essa como aprendizado
+
+    setTypedKeys((preventTypedKeys) =>{
+     
+     return [...preventTypedKeys, key].slice((MAX_TYPED_KEYS) * -1)
+    } ) //vou deixar essa como aprendizado
     // setTYpedKeys([...typedKeys, key]) // porém prefiro essa 
 
   }
-  console.log('oi')
+
+
+  
   return (
     <div className="container" tabIndex='0' onKeyDown={handleKeyDown}>
        <div className='valid-keys'>
-          <span className="matched">emer</span>
-          <span className="remainder">son</span>
+          <Word word={word} validKeys={validKeys}/>
        </div>
        <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
        <div className="completed-words">
